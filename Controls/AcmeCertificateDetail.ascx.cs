052f9e03ac892ee39eed7ea638f99da58a13c457
Daniel Hazelbaker <daniel@blueboxmoon.com>
2017-12-09 12:17:47 -0800

    + Moved the IIS warnings into the Certificate Details instead of the Certificate List.

--- Controls/AcmeCertificateDetail.ascx.cs
+++ Controls/AcmeCertificateDetail.ascx.cs
@@ -118,6 +118,8 @@ namespace RockWeb.Plugins.com_blueboxmoon.AcmeCertificate
 
             ltDetailBindings.Text = string.Join( "<br />", bindings );
 
+            CheckIISState();
+
             pnlEdit.Visible = false;
             pnlDetail.Visible = true;
         }
@@ -147,8 +149,95 @@ namespace RockWeb.Plugins.com_blueboxmoon.AcmeCertificate
 
             GridBind();
 
+            nbIISError.Visible = false;
+            pnlIISRedirectModuleWarning.Visible = false;
+            pnlIISRedirectSiteWarning.Visible = false;
+
             pnlDetail.Visible = false;
             pnlEdit.Visible = true;
+        }
+
+        /// <summary>
+        /// Performs various checks on IIS to ensure it is configured correctly for the certificates that
+        /// have been setup for processing.
+        /// </summary>
+        protected void CheckIISState()
+        {
+            var rockContext = new RockContext();
+            var targetUrl = GetRedirectUrl();
+            var groupTypeId = GroupTypeCache.Read( com.blueboxmoon.AcmeCertificate.SystemGuid.GroupType.ACME_CERTIFICATES ).Id;
+            var bindings = new List<BindingData>();
+
+            var groups = new GroupService( rockContext ).Queryable()
+                .Where( g => g.GroupTypeId == groupTypeId );
+
+            var siteNames = new List<string>();
+
+            //
+            // Determine if we have any certificates that edit bindings of a site other than the Rock site.
+            //
+            foreach ( var group in groups )
+            {
+                var currentSiteName = System.Web.Hosting.HostingEnvironment.SiteName;
+
+                group.LoadAttributes( rockContext );
+
+                bindings.AddRange( group.GetAttributeValue( "Bindings" )
+                    .Split( new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries )
+                    .Select( b => new BindingData( b ) )
+                    .Where( b => b.Site != currentSiteName ) );
+
+                siteNames.AddRange( bindings.Where( b => !AcmeHelper.IsIISSiteRedirectEnabled( b.Site, targetUrl ) ).Select( b => b.Site ) );
+            }
+
+            //
+            // If we have non-Rock sites to configure, ensure that the Http Redirect module has been
+            // installed in IIS.
+            //
+            if ( bindings.Any() && !AcmeHelper.IsHttpRedirectModuleEnabled() )
+            {
+                pnlIISRedirectModuleWarning.Visible = true;
+                pnlIISRedirectSiteWarning.Visible = false;
+            }
+            else
+            {
+                pnlIISRedirectModuleWarning.Visible = false;
+            }
+
+            //
+            // If the redirect module has been installed but we have sites that need to be
+            // configured, then present a notice about those sites.
+            //
+            if ( siteNames.Any() && !pnlIISRedirectModuleWarning.Visible )
+            {
+                siteNames = siteNames.Distinct().ToList();
+
+                hfEnableSiteRedirects.Value = siteNames.AsDelimited( "," );
+                ltEnableSiteRedirects.Text = "<li>" + siteNames.AsDelimited( "</li><li>" ) + "</li>";
+
+                ltTargetRedirect.Text = targetUrl;
+                pnlIISRedirectSiteWarning.Visible = true;
+            }
+            else
+            {
+                pnlIISRedirectSiteWarning.Visible = false;
+            }
+        }
+
+        /// <summary>
+        /// Gets a URL that should be used in configuring the Site Redirects to Rock.
+        /// </summary>
+        /// <returns>A string representing the URL to be used for site redirects.</returns>
+        protected string GetRedirectUrl()
+        {
+            var url = GetAttributeValue( "RedirectOverride" );
+
+            if ( string.IsNullOrWhiteSpace( url ) )
+            {
+                url = string.Format( "{0}.well-known/acme-challenge/", GlobalAttributesCache.Value( "PublicApplicationRoot" ) );
+            }
+
+            return url;
         }
 
         /// <summary>
@@ -251,6 +340,59 @@ namespace RockWeb.Plugins.com_blueboxmoon.AcmeCertificate
             NavigateToParentPage();
         }
 
+        /// <summary>
+        /// Handles the Click event of the lbEnableRedirectModule control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void lbEnableRedirectModule_Click( object sender, EventArgs e )
+        {
+            if ( !AcmeHelper.EnableIISHttpRedirectModule() )
+            {
+                nbIISError.Text = "Failed to enable the IIS Http Redirect module. Rock may not have enough permissions to perform this task. Please manually enable the IIS Http Redirect module.";
+            }
+            else
+            {
+                NavigateToCurrentPage();
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event of the lbEnableSiteRedirects control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void lbEnableSiteRedirects_Click( object sender, EventArgs e )
+        {
+            var siteNames = hfEnableSiteRedirects.Value.Split( ',' );
+            var targetUrl = GetRedirectUrl();
+
+            var errors = new List<string>();
+
+            //
+            // For each site that was detected as not properly configured, try to configure it.
+            //
+            foreach ( var siteName in siteNames )
+            {
+                try
+                {
+                    AcmeHelper.EnableIISSiteRedirect( siteName, targetUrl );
+                }
+                catch ( Exception ex )
+                {
+                    errors.Add( ex.Message );
+                }
+            }
+
+            if ( errors.Any() )
+            {
+                nbIISError.Text = string.Format( "Failed to enable the redirect on one or more sites. This may be due to insufficient permissions to make modifications to the other sites. <ul><li>{0}</li></ul>",
+                    string.Join( "</li><li>", errors ) );
+            }
+
+            CheckIISState();
+        }
+
         #endregion
 
         #region Edit Event Methods
@@ -329,7 +471,14 @@ namespace RockWeb.Plugins.com_blueboxmoon.AcmeCertificate
         /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         protected void lbEditCancel_Click( object sender, EventArgs e )
         {
-            ShowDetail();
+            if ( PageParameter( "Id" ).AsInteger() == 0 )
+            {
+                NavigateToParentPage();
+            }
+            else
+            {
+                ShowDetail();
+            }
         }
 
         /// <summary>
