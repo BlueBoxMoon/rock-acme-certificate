c9f0c341861aea102881a3bfae151ad3d952dbd8
Daniel Hazelbaker <daniel@blueboxmoon.com>
2020-09-12 20:35:11 -0700

    + Fixed error in Rock v11 when Exclusive Routes are enabled.

---
+++ Migrations/005_EnableGlobalRoute.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+using Rock.Plugin;
+
+namespace com.blueboxmoon.AcmeCertificate.Migrations
+{
+    [MigrationNumber( 5, "1.11.0" )]
+    public class EnableGlobalRoute : ExtendedMigration
+    {
+        public override void Up()
+        {
+            Sql( @"UPDATE PR
+SET PR.[IsGlobal] = 1
+FROM [PageRoute] AS PR
+INNER JOIN [Page] AS P ON P.[Id] = PR.[PageId]
+WHERE P.[Guid] = @PageGuid AND PR.[Route] = @Route",
+                new Dictionary<string, object>
+                {
+                    ["PageGuid"] = SystemGuid.Page.ACME_CERTIFICATE_CHALLENGE,
+                    ["Route"] = ".well-known/acme-challenge/{Token}"
+                } );
+        }
+
+        public override void Down()
+        {
+            Sql( @"UPDATE PR
+SET PR.[IsGlobal] = 0
+FROM [PageRoute] AS PR
+INNER JOIN [Page] AS P ON P.[Id] = PR.[PageId]
+WHERE P.[Guid] = @PageGuid AND PR.[Route] = @Route",
+                new Dictionary<string, object>
+                {
+                    ["PageGuid"] = SystemGuid.Page.ACME_CERTIFICATE_CHALLENGE,
+                    ["Route"] = ".well-known/acme-challenge/{Token}"
+                } );
+        }
+    }
+}
